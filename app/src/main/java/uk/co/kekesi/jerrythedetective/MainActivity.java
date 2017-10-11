be08958a3fa789bef4146330cfad2c2f8371e802
Gergo <bluiska@gmail.com>
2017-10-11 16:02:39 +0100

    Fragment style implementation
    Not working just yet...

--- app/src/main/java/uk/co/kekesi/jerrythedetective/MainActivity.java
+++ app/src/main/java/uk/co/kekesi/jerrythedetective/MainActivity.java
@@ -7,19 +7,38 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Layout;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import uk.co.kekesi.jerrythedetective.Fragment.CallsFragment;
+import uk.co.kekesi.jerrythedetective.Fragment.ChatFragment;
+import uk.co.kekesi.jerrythedetective.Fragment.ContactsFragment;
+
 import static android.R.attr.textColor;
 import static uk.co.kekesi.jerrythedetective.R.id.imageView2;
 import static uk.co.kekesi.jerrythedetective.R.id.inv_layout;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+
+
+    //Fragments
+
+    ChatFragment chatFragment;
+    CallsFragment callsFragment;
+    ContactsFragment contactsFragment;
+    MenuItem prevMenuItem;
 
     private TextView mTextMessage;
     private ConstraintLayout mainLayout;
@@ -33,21 +52,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.nav_inventory:
-                    mTextMessage.setText(R.string.title_home);
-                    mainLayout = (ConstraintLayout) findViewById(R.id.container);
-                    view = getLayoutInflater().inflate(R.layout.inventory, mainLayout, false);
-                    mainLayout.addView(view);
-                    inventoryVisible = true;
+                    viewPager.setCurrentItem(0);
                     return true;
                 case R.id.nav_gameplay:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    if(inventoryVisible || mapVisible){
-                        mainLayout.removeView(view);
-                    }
-                    gameplayVisible = true;
+                    viewPager.setCurrentItem(1);
                     return true;
                 case R.id.nav_map:
-                    mTextMessage.setText(R.string.title_notifications);
+                    viewPager.setCurrentItem(2);
                     return true;
             }
             return false;
@@ -59,12 +70,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.nav_gameplay);
 
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
     }
+
+
 
 }
