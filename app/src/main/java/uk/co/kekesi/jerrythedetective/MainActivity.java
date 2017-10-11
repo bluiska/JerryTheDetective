79acf57d147d60cff534ac817a505e58e793fbc5
Gergo <bluiska@gmail.com>
2017-10-11 15:23:22 +0100

    Currently working
    For now the bottom navigation is working. In the development branch I
    will experiment with fragments instead of inflating layouts...

--- app/src/main/java/uk/co/kekesi/jerrythedetective/MainActivity.java
+++ app/src/main/java/uk/co/kekesi/jerrythedetective/MainActivity.java
@@ -1,18 +1,30 @@
 package uk.co.kekesi.jerrythedetective;
 
+import android.annotation.TargetApi;
+import android.content.res.ColorStateList;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import static android.R.attr.textColor;
+import static uk.co.kekesi.jerrythedetective.R.id.imageView2;
+import static uk.co.kekesi.jerrythedetective.R.id.inv_layout;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ConstraintLayout mainLayout;
+    private View view;
+    private boolean inventoryVisible, gameplayVisible, mapVisible;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,16 +32,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.nav_inventory:
                     mTextMessage.setText(R.string.title_home);
-                    ConstraintLayout mainLayout = (ConstraintLayout) findViewById(R.id.container);
-                    View view = getLayoutInflater().inflate(R.layout.inventory, mainLayout,false);
+                    mainLayout = (ConstraintLayout) findViewById(R.id.container);
+                    view = getLayoutInflater().inflate(R.layout.inventory, mainLayout, false);
                     mainLayout.addView(view);
+                    inventoryVisible = true;
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.nav_gameplay:
                     mTextMessage.setText(R.string.title_dashboard);
+                    if(inventoryVisible || mapVisible){
+                        mainLayout.removeView(view);
+                    }
+                    gameplayVisible = true;
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.nav_map:
                     mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
@@ -46,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.nav_gameplay);
+
     }
 
 }
